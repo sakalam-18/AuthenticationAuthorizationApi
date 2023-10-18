2e06608cd038136927f48faf3029fc5fdade6d2f
root <pgeo82@gmail.com>
2023-10-18 10:12:09 +0300

    first commit

---
+++ src/main/java/com/pns/authenticationauthorizationapi/AuthenticationAuthorizationApiApplication.java
@@ -0,0 +1,13 @@
+package com.pns.authenticationauthorizationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAuthorizationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationAuthorizationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pns/authenticationauthorizationapi/AuthenticationAuthorizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pns.authenticationauthorizationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAuthorizationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

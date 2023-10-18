90424878a2d9365bce363730d05e89fd8a2c5231
root <pgeo82@gmail.com>
2023-10-18 13:08:13 +0300

    extractUsername with jwt service

--- src/main/java/com/pns/authenticationauthorizationapi/service/JwtService.java
+++ src/main/java/com/pns/authenticationauthorizationapi/service/JwtService.java
@@ -1,10 +1,39 @@
 package com.pns.authenticationauthorizationapi.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    // get the key from a key generator
+    private static final String SECRET_KEY = "5b947ecac354b635f4a1cb65696b399d85b6979caa9e0f8704eb32bbc0e8a35f";
+
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject); // subject is email or username
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey()) // to verify the sender is who claim to be
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }

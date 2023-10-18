14e4c6bd7fe0432a321b4af8ae68f9bb919f665c
root <pgeo82@gmail.com>
2023-10-18 12:43:20 +0300

    add the jwtAuthentication filter and service

--- src/main/java/com/pns/authenticationauthorizationapi/config/JwtAuthenticationFilter.java
+++ src/main/java/com/pns/authenticationauthorizationapi/config/JwtAuthenticationFilter.java
@@ -1,4 +1,34 @@
 package com.pns.authenticationauthorizationapi.config;
 
-public class JwtAuthenticationFilter {
+import com.pns.authenticationauthorizationapi.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal
+            (@NonNull HttpServletRequest request,
+             @NonNull HttpServletResponse response,
+             @NonNull FilterChain filterChain
+            ) throws ServletException, IOException {
+        final String authHeader  = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response); // go to the next filter
+            return; // to not continue to run the rest code
+        }
+        jwt = authHeader.substring(7); // position seven because is bearer their
+        userEmail = jwtService.extractUsername(jwt);// todo extract the userEmail or username from jwt token ;
+    }
 }
